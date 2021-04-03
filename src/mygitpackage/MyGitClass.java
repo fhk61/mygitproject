abcbad2d404fdc13cee1579048fba6831cc2887f
fhk61 <novictim40@gmail.com>
2021-04-03 19:43:27 +0300

    Second Commit

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,7 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Version1");
-		
+		System.out.println("Second commit");
 	}
 
 }
